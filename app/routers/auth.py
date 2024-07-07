3bad2bf29cb0b7353e9dc73c78f116ea288bb6b4
o3etornam <03etornam@gmail.com>
2024-07-07 05:56:34 +0000

    added login for fastapi docs test

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,3 @@
-from typing import List
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -9,23 +8,84 @@ from .. import utlis
 from .. import oauth2
 
 
-router = APIRouter(
-    prefix='/auth',
-    tags=['Authentication']
-)
+router = APIRouter(tags=["Authentication"])
 
-@router.post('/login', response_model=schema.Token)
-async def login(user_credentials: OAuth2PasswordRequestForm = Depends()
-                ,db:Session = Depends(get_db)):
-    # OAuth2PasswordRequestFor stores what the user enters in a dictionary 
-    # w key words username and password
-    user = db.query(models.User).filter(models.User.email == user_credentials.username)
-    
+
+@router.post("/auth/login", response_model=schema.UserPublic)
+def login(
+    user_credentials: schema.UserLogin,
+    db: Session = Depends(get_db),
+):
+    user = (
+        db.query(models.Users)
+        .filter(models.Users.email == user_credentials.email)
+        .first()
+    )
+
     if user:
-        if await utlis.verify(user_credentials.password, user.password):
-            access_token = await oauth2.create_access_token(data = {'user_id':user.userId})
-            return schema.Token(accessToken=access_token)
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='invalid credentials')
-    
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                         detail=f'user with email {user_credentials.username} not found')
+        if utlis.verify(user_credentials.password, user.password):
+            access_token = oauth2.create_access_token(data={"user_id": user.userId})
+            return schema.UserPublic(
+                message="Login Successful",
+                data=schema.Data(
+                    accessToken=access_token,
+                    user=schema.User(
+                        userId=str(user.userId),
+                        firstName=user.firstName,
+                        lastName=user.lastName,
+                        email=user.email,
+                        phone=user.phone,
+                    ),
+                ),
+            )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "status": "Bad request",
+                "message": "Authentication failed",
+                "statusCode": 401,
+            },
+        )
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail={
+            "status": "Bad request",
+            "message": "Authentication failed",
+            "statusCode": 401,
+        },
+    )
+
+
+@router.post("/login", response_model=schema.TokenData)
+def token_login(
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
+    user = (
+        db.query(models.Users)
+        .filter(models.Users.email == user_credentials.username)
+        .first()
+    )
+
+    if user:
+        if utlis.verify(user_credentials.password, user.password):
+            access_token = oauth2.create_access_token(data={"user_id": user.userId})
+            return schema.TokenData(access_token=access_token)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "status": "Bad request",
+                "message": "Authentication failed",
+                "statusCode": 401,
+            },
+        )
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail={
+            "status": "Bad request",
+            "message": "Authentication failed",
+            "statusCode": 401,
+        },
+    )
