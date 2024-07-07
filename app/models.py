ca54a24eb580136d4c1902cb66a7c415c24b152c
o3etornam <03etornam@gmail.com>
2024-07-07 05:56:07 +0000

    added member table

--- app/models.py
+++ app/models.py
@@ -1,24 +1,41 @@
-from sqlalchemy import DateTime, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-
 from .database import Base
 
 
-class User(Base):
+class Users(Base):
     __tablename__ = "users"
 
-    userId = Column(Integer, primary_key=True, autoincrement=True)
-    firstName = Column(String, unique=True, index=True)
-    lastName = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    created_at = Column(DateTime)
+    userId = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    firstName = Column(String, nullable=False)
+    lastName = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    phone = Column(String)
 
 
-class Organizzation(Base):
-    __tablename__ = "organization"
+class Organisations(Base):
+    __tablename__ = "organisations"
 
-    orgId = Column(Integer, primary_key=True)
-    name = Column(String, index=True)
+    orgId = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, index=True, nullable=False)
     description = Column(String, index=True)
-    ownerId = Column(Integer, ForeignKey("users.userId"))
+
+
+class Members(Base):
+    __tablename__ = "members"
+
+    orgId = Column(
+        Integer,
+        ForeignKey("organisations.orgId", ondelete="cascade"),
+        primary_key=True,
+        nullable=False,
+    )
+    userId = Column(
+        Integer,
+        ForeignKey("users.userId", ondelete="cascade"),
+        primary_key=True,
+        nullable=False,
+    )
+
+    organisation = relationship("Organisations")
