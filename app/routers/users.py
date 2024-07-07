a9187999703221c6ff890c32f94e42f10b8d40dc
o3etornam <03etornam@gmail.com>
2024-07-07 05:55:10 +0000

    fixed user route

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,28 +1,49 @@
-from typing import List
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy import select
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models
 from .. import schema
-from .. import utlis
+from .. import oauth2
 
 
-
-router = APIRouter(
-    prefix='/api',
-    tags= ['Users']
-)
+router = APIRouter(prefix="/api", tags=["Users"])
 
 
-@router.get('/users', response_model= List[schema.User])  
-async def get_users(db:Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return users
-
-@router.get('users/{id}', response_model= schema.User)
-async def get_user(id: int, db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.userId == id).first()
+@router.get("/users/{id}")
+async def get_user(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: schema.User = Depends(oauth2.get_current_user),
+):
+    organisations = (
+        db.query(models.Members)
+        .filter(models.Members.userId == current_user.userId)
+        .all()
+    )
+    user = db.query(models.Users).filter(models.Users.userId == id).first()
     if user:
-        return user
-    raise HTTPException(status_code=404, detail=f'User with id {id} doesn\'t exsit')
+        for org in organisations:
+            if (
+                db.query(models.Members)
+                .filter(
+                    models.Members.orgId == org.orgId,
+                    models.Members.userId == user.userId,
+                )
+                .first()
+            ):
+                return {
+                    "status": "success",
+                    "message": "<message>",
+                    "data": schema.User(
+                        userId=str(user.userId),
+                        firstName=user.firstName,
+                        lastName=user.lastName,
+                        email=user.email,
+                        phone=user.phone,
+                    ),
+                }
+        raise HTTPException(
+            status_code=403, detail=f"Can't access credentials of user with {id}"
+        )
+
+    raise HTTPException(status_code=404, detail=f"User with id {id} doesn't exsit")
