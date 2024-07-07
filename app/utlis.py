87145a18ecd32955c1d06a9b6b5db993aed7835e
o3etornam <03etornam@gmail.com>
2024-07-07 05:56:14 +0000

    .

--- app/utlis.py
+++ app/utlis.py
@@ -2,8 +2,8 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
 
-async def hash(password: str):
+def hash(password: str):
     return pwd_context.hash(password)
 
-async def verify(plain_password,hashed_password):
+def verify(plain_password,hashed_password):
     return pwd_context.verify(plain_password,hashed_password)
